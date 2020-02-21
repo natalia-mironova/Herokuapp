47da4bc03b19d61001a56507815cf954b396a5e3
Natalia <natalia-chekunova@ewave.com>
2020-02-21 21:41:26 +0300

    Dropdowns homework added. Question.

--- src/test/java/Dropdowns.java
+++ src/test/java/Dropdowns.java
@@ -1,37 +1,24 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+/*
+Dropdown - Взять все элементы дроп-дауна и проверить их наличие.
+Выбрать первый, проверить, что он выбран, выбрать второй, проверить, что он выбран
+ */
 
-import java.util.concurrent.TimeUnit;
-
-public class Dropdowns {
+public class Dropdowns extends BeforeAfter {
     @Test
-    public void dropdown1(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
-        browser.manage().window().maximize();
-        browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    public void dropdown1() {
         browser.get("http://the-internet.herokuapp.com/dropdown");
-        browser.findElement(By.id("dropdown")).click();
 
-        WebElement element1 = browser.findElement(By.id("dropdown"));
-        new Select(element1).selectByValue("1");
-        element1.isSelected();
-        System.out.println(element1.isSelected());
-        //Почему false?
+        WebElement element = browser.findElement(By.id("dropdown"));
 
-        WebElement element2 = browser.findElement(By.id("dropdown"));
-        new Select(element2).selectByValue("2");
-        element2.isSelected();
-        System.out.println(element2.isSelected());
+        Select option = new Select(element);
+        option.selectByVisibleText("Option 1"); //по тексту Option 1
+        option.selectByIndex(2); //по индексу Option2
 
-        browser.quit();
-
-        //Тест не падает, но не работает
-
+        //как проверить, выбрана ли опция?
 
     }
 }
