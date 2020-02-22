aab8ca2077c8251bce2fc5118d70e2da7a9c1893
Natalia <natalia-chekunova@ewave.com>
2020-02-22 16:21:09 +0300

    Notifications corrected

--- src/test/java/Notifications.java
+++ src/test/java/Notifications.java
@@ -1,31 +1,21 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
 /*
 кликнуть на кнопку, дождаться появления нотификации, проверить соответствие текста ожиданиям
  */
-public class Notifications {
+public class Notifications extends BeforeAfter {
     @Test
-    public void notification(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver browser = new ChromeDriver();
-        browser.manage().window().maximize();
-        browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    public void notification() {
+
         browser.get("http://the-internet.herokuapp.com/notification_message_rendered");
         browser.findElement(By.linkText("Click here")).click();
 
         String message = browser.findElement(By.id("flash-messages")).getText();
-        System.out.println(message);
-        assertEquals(message, "Action unsuccesful, please try again\n" + "×", "в тексте сейчас что-то другое");
+        assertEquals(message, "Action successful\n" + "×", "в тексте сейчас что-то другое");
+        //или assertEquals(message, "Action unsuccessful, please try again\n" + "×", "в тексте сейчас что-то другое");
 
-        browser.quit();
-
-        //Непонятно как проверять результат
     }
 }
